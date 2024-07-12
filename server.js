dc4b011ee9765d2b01731578cea07ddf7c1bc25e
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-07-12 13:54:48 -0300

    Instalando outras dependências e authenticando conexão com bd

--- server.js
+++ server.js
@@ -3,11 +3,33 @@
 const express = require('express')
 const app = express()
 const sequelize = require('sequelize')
+const bodyparser = require('body-parser')
 
+
+//IMPORTANDO CONEXÃO
+const connection = require('./src/connection/connection')
+
+//AUTENTICANDO CONEXÃO COM BANCO DE DADOS
+connection.authenticate()
+    .then(() =>{
+        console.log('banco de dados social-meida conectado')
+    })
+    .catch((erro) =>{
+        console.log(`erro ${erro}`)
+    })
 
 app.get('/', (req, res) =>{
     
 })
+
+
+
+//USANDO MIDDLEWARE BODY-PARSER
+app.use(bodyparser.urlencoded({
+    extended: false
+}))
+app.use(bodyparser.json())
+
 
 
 //ABRINDO SERVER
--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,4 +1,10 @@
 
-const express = require('express')
 const sequelize = require('sequelize')
 
+const connection = new sequelize('social-media', 'root', 'bravogamessempre123', {
+    host: 'localhost',
+    dialect: 'mysql',
+    timezone: '-03:00'
+})
+
+module.exports = connection
