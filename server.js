2cf7aec26856b7b3c238a9fe1bc701c378802b4b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-07-12 13:39:46 -0300

    Instalando sequelize

---
+++ server.js
@@ -0,0 +1,16 @@
+
+//INSTALANDO DEPÊNDENCIAS
+const express = require('express')
+const app = express()
+const sequelize = require('sequelize')
+
+
+app.get('/', (req, res) =>{
+    
+})
+
+
+//ABRINDO SERVER
+app.listen(8500, ()=>{
+    console.log('aplicação rodando')
+})
